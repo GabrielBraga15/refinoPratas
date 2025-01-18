b650b91a1ef0c67ef04b81f7168156fc9d80b4a4
Gabriel Braga <gabrielferreira9034@gmail.com>
2025-01-18 16:27:38 -0300

    Primeiro commit do projeto Refino Pratas

--- src/app/utils/googleSheets.ts
+++ src/app/utils/googleSheets.ts
@@ -1,18 +1,18 @@
 // src/utils/googleSheets.ts ou no arquivo onde está a função getGoogleSheetsClient
-
+import dotenv from 'dotenv';
+dotenv.config();
 import { google } from 'googleapis';
 
 
 const SPREADSHEET_ID = '1Q20g2hLf1D84p6FOG1jf_gIsVtcVGQyBKpmPnxTx9Vg';
-
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
 const getGoogleSheetsClient = async () => {
     const auth = new google.auth.GoogleAuth({
         credentials: {
             private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
             client_email: process.env.GOOGLE_CLIENT_EMAIL,
         },
-        projectId: process.env.GOOGLE_PROJECT_ID,
-        scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+        scopes: SCOPES,
     });
 
     return google.sheets({ version: 'v4', auth });
